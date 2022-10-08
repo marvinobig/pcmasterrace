64000be35ea6bd5906745af6b3755cceb16f38c2
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 15:53:13 +0100

    create homepage

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php require "config/bootstrap.php";?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,13 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PC Master Race</title>
+    <link rel="stylesheet" href="static/css/reset.css">
+    <link rel="stylesheet" href="static/css/style.css">
+    <script src="static/js/script.js" defer></script>
 </head>
 
 <body>
-    <?php require "$cwd/pages/partials/nav.partial.php";?>
-</body>
-<main>
+    <nav>
+        <a class="active" href="index.php">Home</a>
+        <a href="pages/login.page.php">Login</a>
 
-</main>
+        <div class="nav-right">
+            <a href="pages/register.page.php">Register</a>
+        </div>
+    </nav>
+    <main>
+
+    </main>
+    <?php include "pages/partials/footer.partial.php"?>
+</body>
 
 </html>
