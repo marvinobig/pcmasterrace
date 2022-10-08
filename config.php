0890ded726edfd5fe4d55b8c39acf73f97f4469d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 15:53:01 +0100

    add db configs

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     "database" => [
-        "name" => "tasks",
+        "name" => "pc_master_race",
         "username" => "root",
         "password" => "",
         "connection" => "mysql:host=127.0.0.1;",
