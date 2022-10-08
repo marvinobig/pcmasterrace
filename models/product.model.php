e72a2eca410d1c413f9b88340602cd9f11abfa3b
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 15:52:25 +0100

    create product model

---
+++ models/product.model.php
@@ -0,0 +1,48 @@
+<?php
+
+class Product
+{
+    protected $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function PostProduct(string $name, string $description, float $price, string $image)
+    {
+        try {
+            $sql = $this->pdo->prepare("INSERT INTO products(name, description, price, image) VALUES (?, ?, ?, ?)");
+
+            $sql->execute(["$name", "$description", $price, "$image"]);
+        } catch (Exeception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function GetAllProducts(): array
+    {
+        try {
+            $sql = $this->pdo->prepare("SELECT * FROM products");
+
+            $result = $sql->execute();
+
+            return $sql->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exeception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function GetProductById(int $id): array
+    {
+        try {
+            $sql = $this->pdo->prepare("SELECT * FROM products WHERE ID = ?");
+
+            $result = $sql->execute([$id]);
+
+            return $sql->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exeception $e) {
+            die($e->getMessage());
+        }
+    }
+}
