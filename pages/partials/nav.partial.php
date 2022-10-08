42072d1e6bac9670af6058245d731950ccea80cc
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 16:23:46 +0100

    add product page link

--- pages/partials/nav.partial.php
+++ pages/partials/nav.partial.php
@@ -1,10 +1,7 @@
 <nav>
     <a class="active" href="../../index.php">Home</a>
     <a href="login.page.php">Login</a>
-
-    <div class="nav-right">
-        <a href="register.page.php">Register</a>
-    </div>
-
+    <a href="register.page.php">Register</a>
+    <a href="products.page.php">Products</a>
     <a href="basket.page.php">Basket</a>
 </nav>
