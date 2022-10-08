0ce6502cf623bae624122f2c7427351438634a0b
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 15:53:41 +0100

    view single product

---
+++ pages/viewProduct.page.php
@@ -0,0 +1,44 @@
+<?php
+$root = $_SERVER["DOCUMENT_ROOT"];
+require "$root/core/bootstrap.php";
+
+$product = new Product($pdo);
+$id = $_GET["id"];
+
+// session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PC Master Race</title>
+    <link rel="stylesheet" href="../static/css/reset.css">
+    <link rel="stylesheet" href="../static/css/style.css">
+    <script src="../static/js/script.js" defer></script>
+</head>
+
+<body>
+    <?php require "$root/pages/partials/nav.partial.php";?>
+
+    <main>
+        <?php foreach ($product->GetProductById($id) as $item): ?>
+        <img src="Images/<?=$item["image"]?>">
+        <h2><?=$item["name"]?></h2>
+        <p>£<?=$item["price"]?></p>
+        <h3>Description</h3>
+        <p><?=$item["description"]?></p>
+        <a href="../functions/addToBasket.function.php?ID='<?=$item['ID']?>'">
+            <button type="button">
+                Add to basket
+            </button>
+        </a>
+        <?php endforeach?>
+    </main>
+
+    <?php include "$root/pages/partials/footer.partial.php";?>
+</body>
+
+</html>
