e27b2ae99b8b3f943a5840a4acd1501a0774e282
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-08 15:54:01 +0100

    create nav & footer partials

---
+++ pages/partials/footer.partial.php
@@ -0,0 +1,13 @@
+<footer>
+    <h2>Marvin Obigwilo</h2>
+    <div>
+        <p>PC Master Race Website</p>
+    </div>
+
+    <div>
+        <h2>Socials</h2>
+        <a href="https://github.com/marvinobig" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://marvinobig.github.io" target="_blank" rel="noreferrer">Portfolio</a>
+        <a href="https://www.linkedin.com/in/marvin-obigwilo/" target="_blank" rel="noreferrer">LinkedIn</a>
+    </div>
+</footer>
--- pages/partials/nav.partial.php
+++ pages/partials/nav.partial.php
@@ -1,9 +1,10 @@
-<?php require "config/bootstrap.php";?>
 <nav>
-    <a class="active" href=<?="$cwd/index.php"?>>Home</a>
-    <a href=<?="$cwd/pages/login.pages.php"?>>Login</a>
+    <a class="active" href="../../index.php">Home</a>
+    <a href="login.page.php">Login</a>
 
     <div class="nav-right">
-        <a href=<?="$cwd/pages/register.pages.php"?>>Register</a>
+        <a href="register.page.php">Register</a>
     </div>
+
+    <a href="basket.page.php">Basket</a>
 </nav>
