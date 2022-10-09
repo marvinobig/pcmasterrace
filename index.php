3bf85f26b6d92a543519c009cc42e6c30cd00639
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-09 12:44:32 +0100

    styled nav bar

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
 <?php require "pages/partials/head.partial.php";?>
 
 <body>
-    <nav>
-        <a class="active" href="index.php">Home</a>
+    <nav class="nav">
+        <a class="nav-links" href="index.php">Home</a>
 
-        <a href="pages/products.page.php">Products</a>
+        <div class="nav-right">
+            <a class="nav-links" href="pages/products.page.php">Products</a>
 
-        <?php if (!isset($_SESSION["username"])): ?>
-        <a href="pages/login.page.php">Login</a>
-        <a href="pages/register.page.php">Register</a>
-        <?php else: ?>
-        <a href="pages/account.page.php">Account</a>
-        <a href="pages/basket.page.php">Basket</a>
-        <?php endif?>
+            <?php if (!isset($_SESSION["username"])): ?>
+            <a class="nav-links" href="pages/login.page.php">Login</a>
+            <a class="nav-links" href="pages/register.page.php">Register</a>
+            <?php else: ?>
+            <a class="nav-links" href="pages/account.page.php">Account</a>
+            <a class="nav-links" href="pages/basket.page.php">Basket</a>
+            <?php endif?>
+        </div>
     </nav>
 
     <main>
--- pages/partials/nav.partial.php
+++ pages/partials/nav.partial.php
@@ -1,13 +1,16 @@
-<nav>
-    <a class="active" href="../../index.php">Home</a>
+<nav class="nav">
+    <a class="nav-links" href="../../index.php">Home</a>
 
-    <a href="products.page.php">Products</a>
+    <div class="nav-right">
+        <a class="nav-links" href="products.page.php">Products</a>
 
-    <?php if (!isset($_SESSION["username"])): ?>
-    <a href="login.page.php">Login</a>
-    <a href="register.page.php">Register</a>
-    <?php else: ?>
-    <a href="account.page.php">Account</a>
-    <a href="basket.page.php">Basket</a>
-    <?php endif?>
+        <?php if (!isset($_SESSION["username"])): ?>
+        <a class="nav-links" href="login.page.php">Login</a>
+        <a class="nav-links" href="register.page.php">Register</a>
+
+        <?php else: ?>
+        <a class="nav-links" href="account.page.php">Account</a>
+        <a class="nav-links" href="basket.page.php">Basket</a>
+        <?php endif?>
+    </div>
 </nav>
