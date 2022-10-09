b111441eb298c71a6f4f821bea682a7d3a2842ef
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-09 13:04:53 +0100

    style footer

--- pages/partials/footer.partial.php
+++ pages/partials/footer.partial.php
@@ -1,13 +1,15 @@
-<footer>
-    <h2>Marvin Obigwilo</h2>
-    <div>
-        <p>PC Master Race Website</p>
+<footer class="footer">
+    <div class="footer-title-container">
+        <h2 class="footer-title">PC Master Race E-Commerce Website</h2>
+        <h3 class="footer-title">By Marvin Obigwilo</h3>
     </div>
 
-    <div>
-        <h2>Socials</h2>
-        <a href="https://github.com/marvinobig" target="_blank" rel="noreferrer">Github</a>
-        <a href="https://marvinobig.github.io" target="_blank" rel="noreferrer">Portfolio</a>
-        <a href="https://www.linkedin.com/in/marvin-obigwilo/" target="_blank" rel="noreferrer">LinkedIn</a>
+    <div class="footer-socials-container">
+        <h2 class="footer-socials-title">Socials</h2>
+        <a class="footer-socials-link" href="https://github.com/marvinobig" target="_blank" rel="noreferrer">Github</a>
+        <a class="footer-socials-link" href="https://marvinobig.github.io" target="_blank"
+            rel="noreferrer">Portfolio</a>
+        <a class="footer-socials-link" href="https://www.linkedin.com/in/marvin-obigwilo/" target="_blank"
+            rel="noreferrer">LinkedIn</a>
     </div>
 </footer>
