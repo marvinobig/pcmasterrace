9716410187afc049e944d2b6d0df10b43d1e341d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-10-13 14:25:17 +0100

    style home page

--- index.php
+++ index.php
@@ -21,10 +21,32 @@
         </div>
     </nav>
 
-    <main>
+    <main class="home">
         <p class="welcome">
-            Welcome to PC Master Race. Sign up and buy your pc parts now.
+            Welcome to PC Master Race. Sign up and buy your pc parts now. Whatever your passion, be it PC gaming,
+            professional graphics, video editing, music production, high performance compute or artificial intelligence
         </p>
+
+        <section class="site-info">
+            <div class="site-info-card">
+                <h2>Satisfaction</h2>
+                <p>
+                    Our depth of understanding and guidance results in 100% customer satisfaction.
+                </p>
+            </div>
+            <div class="site-info-card">
+                <h2>Specification</h2>
+                <p>
+                    We provide Computers, Components, Gaming, Audio, Video, Graphics.
+                </p>
+            </div>
+            <div class="site-info-card">
+                <h2>Customer Service</h2>
+                <p>
+                    We are passionate about giving people what they need, building relationships and nurturing loyalty.
+                </p>
+            </div>
+        </section>
 
     </main>
 
